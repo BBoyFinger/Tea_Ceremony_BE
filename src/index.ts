cf34af1b52bee4a59aaed6fd327d36852747beb1
Vũ Ngọc Tú <80776665+tuvngch18505@users.noreply.github.com>
2024-10-24 23:30:54 +0700

    update COrrs

--- src/index.ts
+++ src/index.ts
@@ -11,26 +11,17 @@ dotenv.config();
 // Create an Express application
 const app = express();
 
-app.use(express.json({ limit: '50mb' })); // Tăng giới hạn kích thước cho JSON payload
-app.use(express.urlencoded({ limit: '50mb', extended: true })); // Tăng giới hạn cho URL-encoded payload
+app.use(express.json({ limit: "50mb" })); // Tăng giới hạn kích thước cho JSON payload
+app.use(express.urlencoded({ limit: "50mb", extended: true })); // Tăng giới hạn cho URL-encoded payload
 
 // Define the port number from environment variable or default to 8081
 const PORT: number = parseInt(process.env.PORT || "8081", 10);
 
-const allowedOrigins = ['https://tea-ware-fe.vercel.app'];  
-
-const corsOptions = {
-  origin: function (origin: any, callback: any) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
- 
-// Middleware to parse JSON
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
