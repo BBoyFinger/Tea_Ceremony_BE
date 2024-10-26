c4b5c47d6d3ecff442be0127563e6359f04796f5
BBoyFinger <tuvngdevsl@gmail.com>
2024-10-26 17:19:01 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,8 @@ const corsOptions = {
   credentials: true,  // Cho phép gửi thông tin xác thực (cookies, etc.)
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors({ origin: "http://localhost:3000" , credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
